4f7add0f4dfcc088cb9cb5dfd369b37ea1b84794
Ladd Laulusa <laddlaulusa@gmail.com>
2015-11-20 14:28:16 -0700

    notes

--- magic_eight.rb
+++ magic_eight.rb
@@ -1,15 +1,22 @@
-# Projects:
-#   - Magic Eight Ball
-
-# Descriptions:
-#   - Magic Eight Ball: Ask the ball a question and get a random answer. (Bonus objectives)
-#  
 # Magic Eight Ball Pseudocode:
 # Basic Objectives:
 # - User inputs question
 # - Computer outputs random answer
 # - User inputs "QUIT"
 # - Computer outputs a goodbye message and exits
+
+# Bonus Objectives:
+# - ability to add more answers:
+#   - via easter egg question ("add_answers")
+#   - via script arguments (ruby magic_eight.rb add_answers)
+#   - do not let them add the same answer if the eight ball already has that answer
+
+# - ability to reset answers back to original bank (hint: think constants)
+#   - via easter egg question ("reset_answers")
+
+# - ability to have eight ball print all answers
+#   - via easter egg question ("print_answers")
+
 
 def main_menu
 	puts "welcome to the magic 8 ball."
